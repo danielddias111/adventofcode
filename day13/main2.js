0df5408ae479c271253e62b5d62623601f8bfa60
Daniel Dias <danieldias111@hotmail.com>
2020-12-14 07:31:37 +0000

    day13

--- day13/main2.js
+++ day13/main2.js
@@ -1,39 +1,74 @@
 var fs = require('fs');
 
+let time = 100000000000000
+let validBus = []
+let nextBus
+let maxValue = 0
+let value = 0
+let mapValue = new Map()
 const getBus = (bus) => {
-    let time = 100000000000000
     let allBus = bus[1].split(',')
-    let validBus = []
-    let nextBus
-    let lastValid = 0
     for (let i = 0; i < allBus.length; i++) {
         if (allBus[i] != 'x') {
             validBus.push(parseInt(allBus[i]))
+            if (parseInt(allBus[i]) > maxValue) {
+                maxValue = parseInt(allBus[i])
+            }
         }
+    }
+    while(true){
+        if(time % maxValue == 0){
+            value = time
+            break
+        }
+        time++
     }
     nextBus = validBus[0]
-    for(let i=0; i< allBus.length;i++){
-        if(allBus[i] == 'x'){
-            time++
-            continue
-        }
-        else {
-            if(time % allBus[i] == 0){
-                if(i==0){
-                    lastValid = time
-                }
-                time++
-                continue
-            }
-            else {
-                time = lastValid+validBus[0]
-                i=-1
+    while (true) {
+        if (time % maxValue == 0) {
+            let allValid = validate(allBus, 0)
+            if (allValid) {
+                return time - allBus.indexOf(String(maxValue))
             }
         }
+        
+        if(time % value  == 0){
+            console.log('One Lap')
+        }
+        time += maxValue
     }
-    return time - allBus.length
 }
 
+const validate = (allBus, idx) => {
+    let counter = 0
+    if (mapValue.get(idx) == null) {
+        for (let i = 0; i < allBus.length; i++) {
+            if (i == idx) {
+                break
+            }
+            counter++
+            mapValue.set(idx, counter)
+        }
+    }
+    else {
+        counter = mapValue.get(idx)
+    }
+
+
+    let difference = allBus.indexOf(String(maxValue)) - allBus.indexOf(allBus[counter])
+
+    if ((time - difference) % parseInt(allBus[counter]) == 0) {
+        if (parseInt(allBus[counter]) == validBus[validBus.length - 1]) {
+            return true
+        }
+        let value = parseInt(allBus[counter])
+        let nextValue = allBus.indexOf(String(validBus[validBus.indexOf(value) + 1]))
+        return validate(allBus, nextValue)
+    }
+    else {
+        return false
+    }
+}
 
 
 

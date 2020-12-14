e448197e42366aadb15447ec5d2c42d65907b0d4
Daniel Dias <danieldias111@hotmail.com>
2020-12-14 20:47:01 +0000

    day14 part I

---
+++ day14/main2.js
@@ -0,0 +1,55 @@
+var fs = require('fs');
+
+let mask
+let myMap = new Map()
+let memoryIdx
+let mem
+let value
+let tempValue
+let binary
+const memorySum = (memory) => {
+    for (let i = 0; i < memory.length; i++) {
+        if (memory[i].trim() == '') {
+            break
+        }
+        mem = memory[i].split('=')[0].replace(' ', '')
+        value = memory[i].split('=')[1].replace(' ', '')
+        if (mem == 'mask') {
+            mask = value
+            //console.log(mask)
+        }
+        else {
+            tempValue = ''
+            memoryIdx = mem.split('[')[1].split(']')[0]
+            binary = Number(value).toString(2)
+            for (let x = 0; x < mask.length; x++) {
+                if (binary.length > x && mask[mask.length - x - 1] == 'X') {
+                    tempValue = binary[binary.length - x - 1] + tempValue
+                }
+                else {
+                    if (mask[mask.length - x - 1] == 'X') {
+                        tempValue = '0' + tempValue
+                    }
+                    else {
+                        tempValue = mask[mask.length - x - 1] + tempValue
+                    }
+                }
+            }
+            myMap.set(memoryIdx, tempValue)
+        }
+    }
+
+    let number = 0
+    let count=0
+    for (const [key, value] of myMap.entries()) {
+        number = parseInt(value,2)
+        count+=number
+    }
+    return count
+}
+
+const data = fs.readFileSync('./day14/input.txt', 'UTF-8');
+const lines = data.split(/\r?\n/);
+
+let result = memorySum(lines)
+console.log('Result:', result)
